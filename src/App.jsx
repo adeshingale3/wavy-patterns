476c17786244b6ed9c39f077eb7102a961a080e2
adeshingale3 <adeshingale600@gmail.com>
2025-08-05 13:26:06 +0530

    FadingBG added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FadingBG from './compnents/FadingBG'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen'>
+      <FadingBG />
+    </div>
+  )
+}
+
+export default App
